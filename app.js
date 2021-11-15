89fff8eb0328e2cbd0fbadfcc36df04d37205f6c
Erik Mota Galindo <kenshimota@gmail.com>
2021-11-15 13:12:56 -0400

    change files to babel process

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const express = require('express');
+import path from 'path';
+import cors from 'cors';
+import express from 'express';
+import history from 'connect-history-api-fallback';
+
 const app = express();
 const port = 3000;
 
-
-// evitando el problema de cors origin
-const cors = require('cors');
+// configurando el servidor del historial y renderizado de vue
 app.use(cors());
-
-// Para acceder al directorio actual
-const path = require('path');
+app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // estableciendo porque puerto escuchara la aplicacion determinada
 app.listen(port, function(){
 	console.log("Ejemplo del listen de una app web por el puerto "  + port);
 });
-
-// Middleware para Vue.js router modo history
-const history = require('connect-history-api-fallback');
-app.use(history());
-app.use(express.static(path.join(__dirname, 'public')));
-
