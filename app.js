6883495c05e6bcb6bfe7f1c7de116b564e85bf86
Erik Mota Galindo <kenshimota@gmail.com>
2021-11-15 12:44:30 -0400

    add function cors

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 
 
+// evitando el problema de cors origin
+const cors = require('cors');
+app.use(cors());
+
+
 // estableciendo porque puerto escuchara la aplicacion determinada
 app.listen(port, function(){
 	console.log("Ejemplo del listen de una app web por el puerto "  + port);
